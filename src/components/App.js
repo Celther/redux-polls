0d0ef85778262456676326d2cc2c723a8da3199f
Celther <mwfister@gmail.com>
2018-07-17 23:55:42 +1000

    Refactoring - Sorted polls for Dashboard
    Sorting the polls by AUTH user answers before passing it in as props.

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import { handleInitialData } from '../actions/shared'
-import Polls from './Polls'
+import Dashboard from './Dashboard'
 
 class App extends Component {
   componentDidMount () {
@@ -12,11 +12,18 @@ class App extends Component {
   render () {
     return (
       <div>
-        Starter Code.
-        <Polls />
+        {this.props.loading
+          ? null
+          :<Dashboard />}
       </div>
     )
   }
 }
 
-export default connect()(App)
+function mapStateToProps ({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
---
+++ src/components/Dashboard.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class Dashboard extends Component {
+  render () {
+    console.log(this.props);
+    return (
+      <div>
+        Dashboard
+      </div>
+    )
+  }
+}
+
+function mapStateToProps ({ authedUser, polls, users }) {
+  const answers = Object.keys(users[authedUser].answers)
+
+  const answered = answers.map((id) => polls[id])
+    .sort((a,b) => b.timestamp - a.timestamp)
+
+  const unanswered = Object.keys(polls)
+    .filter((id) => !answers.includes(id))
+    .map((id) => polls[id])
+    .sort((a,b) => b.timestamp - a.timestamp)
+
+  return {
+    answered,
+    unanswered,
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard)
