95780cb15bc25f5a0f5e92b75186085b555e97bc
mwfister <mwfister@gmail.com>
2018-07-17 16:09:05 +1000

    Logger/Thunk middleware
    Custom logger displays actions and the state for each action type.
    
    Thunk to intercept Function dispatches. (Async)

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import { createStore } from 'redux'
 import './index.css'
 import App from './components/App'
 import reducer from './reducers'
+import middleware from './middleware'
 
-const store = createStore(reducer)
+const store = createStore(reducer, middleware)
 
 ReactDOM.render(
   <Provider store={store}>
