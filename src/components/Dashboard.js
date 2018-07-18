9b76c6994d6cb88ed7c6cad52210bdb61aa6d9fd
Celther <mwfister@gmail.com>
2018-07-18 18:38:02 +1000

    Render (Un)answered questions
    Toggles between which polls AUTHED_USER has and hasn't yet answered.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+import List from './List'
+
 class Dashboard extends Component {
+  state = {
+    showAnswered: false
+  }
+  showUnanswered = () => {
+    this.setState(() => ({
+      showAnswered: false
+    }))
+  }
+  showAnswered = () => {
+    this.setState(() => ({
+      showAnswered: true
+    }))
+  }
+
   render () {
-    console.log(this.props);
+    const { showAnswered } = this.state
+    const { answered, unanswered } = this.props
+    
     return (
-      <div>
-        Dashboard
+      <div className="container">
+        <div className="dashboard-toggle">
+          <button
+            style={{textDecoration: !showAnswered ? 'underline' : null}}
+            onClick={this.showUnanswered}
+          >
+            Unanswered
+          </button>
+          <span> | </span>
+          <button
+            style={{textDecoration: showAnswered ? 'underline' : null}}
+            onClick={this.showAnswered}
+          >
+            Answered
+          </button>
+        </div>
+        {showAnswered
+          ? <List items={answered} />
+          : <List items={unanswered} />}
       </div>
     )
   }
