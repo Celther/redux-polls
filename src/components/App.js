a88fbc08adc7104ffccc00312680d036bedfdcb2
mwfister <mwfister@gmail.com>
2018-07-19 11:20:28 +1000

    Temp. rendering Leaderboard on homepage

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import LoadingBar from 'react-redux-loading'
 
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
+import Leaderboard from './Leaderboard'
 
 class App extends Component {
   componentDidMount () {
@@ -16,7 +17,10 @@ class App extends Component {
         <LoadingBar />
         {this.props.loading
           ? null
-          : <Dashboard />}
+          : <div>
+              <Dashboard />
+              <Leaderboard />
+            </div>}
       </div>
     )
   }
