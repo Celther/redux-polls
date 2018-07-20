b3769ffeffd69da3e70631274ab578d73c143b9e
mwfister <mwfister@gmail.com>
2018-07-20 17:11:26 +1000

    Poll Answer structure

--- src/actions/polls.js
+++ src/actions/polls.js
@@ -1,8 +1,9 @@
-import { savePoll } from '../utils/api'
+import { savePoll, savePollAnswer } from '../utils/api'
 import { hideLoading, showLoading } from 'react-redux-loading'
 
 export const RECEIVE_POLLS = 'RECEIVE_POLLS'
 export const ADD_POLL = 'ADD_POLL'
+export const ADD_POLL_ANSWER = 'ADD_POLL_ANSWER'
 
 export function receivePolls (polls) {
   return {
@@ -11,7 +12,7 @@ export function receivePolls (polls) {
   }
 }
 
-export function addPoll (poll) {
+function addPoll (poll) {
   return {
     type: ADD_POLL,
     poll,
@@ -30,5 +31,23 @@ export function handleAddPoll (poll) {
       .then((p) => dispatch(addPoll(p)))
       .then(() => dispatch(hideLoading()))
       .catch((e) => alert(`Error: ${e.message || e}`))
+  }
+}
+
+function addPollAnswer (args) {
+  return {
+    type: ADD_POLL_ANSWER,
+    args,
+  }
+}
+
+export function handleAddPollAnswer (poll) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState()
+    const { id, answer } = poll
+    const args = { authedUser, id, answer }
+
+    savePollAnswer(args)
+      .then(() => addPollAnswer(args))
   }
 }
--- src/reducers/polls.js
+++ src/reducers/polls.js
@@ -1,6 +1,7 @@
 import {
   RECEIVE_POLLS,
   ADD_POLL,
+  ADD_POLL_ANSWER,
  } from '../actions/polls'
 
 export default function polls (state = {}, action) {
@@ -14,6 +15,16 @@ export default function polls (state = {}, action) {
       return {
         ...state,
         [action.poll.id]: action.poll,
+      }
+    case ADD_POLL_ANSWER:
+      const { id, authedUser, answer } = action
+      const votes = answer + 'votes'
+      return {
+        ...state,
+        [id]: {
+          ...state[id],
+          [votes]: state[id][votes].concat(authedUser)
+        }
       }
     default:
       return state
