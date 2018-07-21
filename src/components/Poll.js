8adb257485f2c5bfc776b01355ceb6587cf6f583
Celther <mwfister@gmail.com>
2018-07-21 17:48:58 +1000

    Finding AUTHED_USER vote
    Using reducer to read over poll's votes, returning the choice the AUTHED_USER made. null if none.

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -16,6 +16,24 @@ function mapStateToProps ({ authedUser, polls, users }, { match }) {
   const poll = polls[id]
   const author = users[poll.author]
   const user = users[authedUser]
+  // If user tries to access a poll that doesn't exist
+  if (!poll) {
+    return {
+      poll: null
+    }
+  }
+
+  const vote = ['aVotes', 'bVotes', 'cVotes', 'dVotes'].reduce((vote, key) => {
+    if (vote !== null) {
+      return vote[0]
+    }
+
+    return poll[key].includes(authedUser)
+      ? key
+      : vote
+  }, null)
+
+
 
   return {
     poll,
