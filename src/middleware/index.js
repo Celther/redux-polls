d9d759555eacddbaaaa40895655046eb4fb834fc
Celther <mwfister@gmail.com>
2018-07-22 17:35:15 +1000

    Redux Devtools
    Requires browser extension to view:
    
    https://github.com/zalmoxisus/redux-devtools-extension

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,6 +1,8 @@
-import { applyMiddleware } from 'redux'
+import { applyMiddleware, compose } from 'redux'
 import ReduxThunk from 'redux-thunk'
 
 import logger from './logger'
 
-export default applyMiddleware(ReduxThunk, logger)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default composeEnhancers(applyMiddleware(ReduxThunk, logger))
